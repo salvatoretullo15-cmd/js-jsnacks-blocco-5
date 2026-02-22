45c1722c989d9faf5079318942592d5afddca7a8
salvatore tullo <salvatoretullo15@gmail.com>
2026-02-22 17:14:28 +0100

    commit settimo esercizio completato

--- ex-js-array-methods-main/snack-7/script.js
+++ ex-js-array-methods-main/snack-7/script.js
@@ -21,3 +21,10 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+
+console.log(students[1]);
+/*
+qui abbiamo loggato solo il secondo oggetto dell'arrey nella sua interezza 
+*/
+
