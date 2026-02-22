7522811d5b54ca929788bc25244040d79ec63f06
salvatore tullo <salvatoretullo15@gmail.com>
2026-02-22 17:42:28 +0100

    commit sesto snack completato

--- ex-js-array-methods-main/snack-6/script.js
+++ ex-js-array-methods-main/snack-6/script.js
@@ -11,4 +11,9 @@ const zucchine = [
   { type: 'Calabrese', weight: 14, length: 4 },
 ];
 
-// A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+// A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.//
+
+const longZucchine = zucchine.filter(zucchina => zucchina.length >= 15)
+console.log(longZucchine);
+const shortZucchine = zucchine.filter(zucchina => zucchina.length <= 15)
+console.log(shortZucchine);
