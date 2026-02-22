1a413a803bf5413822fadf247d865804ebac1174
salvatore tullo <salvatoretullo15@gmail.com>
2026-02-22 16:47:10 +0100

    commit secondo snack completato

--- ex-js-array-methods-main/snack-2/script.js
+++ ex-js-array-methods-main/snack-2/script.js
@@ -6,3 +6,20 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+people.forEach(person => {
+  console.log(person.name);
+});
+
+/*
+people.forEach():Questo metodo ci dice di "passare in rassegna" 
+ogni elemento contenuto nell'array "people",uno alla volta 
+*/
+/*
+person:È il nome che diamo alla variabile temporanea che rappresenta l'elemento 
+corrente durante il ciclo
+*/
+/*
+person.name:Poiché ogni elemento è un oggetto, usiamo la "dot notation" (il punto) 
+per accedere solo alla proprietà - name -
+*/ 
