739e9c55539f793173f50fff92981c67f95e08bd
salvatore tullo <salvatoretullo15@gmail.com>
2026-02-23 09:40:03 +0100

    correzione esercizio 1

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -3,4 +3,11 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
-console.log(names);
+
+names.forEach((name)=>{  //correzione fatta da fabio 
+    console.log(name);
+})
+
+
+
+ console.log(names); //quello che avevo fatto io 
